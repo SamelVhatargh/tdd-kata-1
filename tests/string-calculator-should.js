aa59b640e30e0a39c4883928990973b44fe13d74
Samel Vhatargh <samel@bk.ru>
2017-02-20 23:33:12 +1100

    removed unnecessary tests

--- tests/string-calculator-should.js
+++ tests/string-calculator-should.js
@@ -16,28 +16,12 @@ suite('String calculator should', function () {
         assert.equal(result, 0);
     });
 
-    test('return 1 if input is 1', function () {
-        let calculator = createCalculator();
-
-        let result = calculator.add('1');
-
-        assert.equal(result, 1);
-    });
-
     test('return one-digit number if input is that number', function () {
         let calculator = createCalculator();
 
         let result = calculator.add('3');
 
         assert.equal(result, 3);
-    });
-
-    test('return sum of two ond-digit numbers', function () {
-        let calculator = createCalculator();
-
-        let result = calculator.add('3,2');
-
-        assert.equal(result, 3 + 2);
     });
 
     test('return sum of any two numbers', function () {
